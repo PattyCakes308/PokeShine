3975b88b967f4488ebeda50f12cc93e57445976d
PattyCakes308 <patrickg308@gmail.com>
2023-11-06 03:48:18 -0500

    hmmm

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 <h2>${name}</h2>
                 <p>Height: ${height} decimetres</p>
                 <p>Weight: ${weight} hectograms</p>
-                <img id="pokemon-image" src="" alt="Pokemon Image">
             `;
 
             // Update the content of the div with the Pokemon data
