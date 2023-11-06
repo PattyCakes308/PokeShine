ec01e651b87a14960c4fa3806f70abef356714e1
PattyCakes308 <patrickg308@gmail.com>
2023-11-06 03:30:25 -0500

    checking on displaying images

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Element where we will display the Pokemon data
+    // Elements where we will display the Pokemon data and image
     const pokemonDataElement = document.getElementById("pokemon-data");
+    const pokemonImageElement = document.getElementById("pokemon-image");
 
     // URL for the PokeAPI to retrieve data for a specific Pokémon (e.g., Pikachu)
     const pokemonName = "pikachu";
@@ -15,6 +16,9 @@ document.addEventListener("DOMContentLoaded", function () {
             const height = data.height;
             const weight = data.weight;
 
+            // Extract the URL of the Pokémon's image
+            const imageUrl = data.sprites.front_default;
+
             // Create HTML content to display the data
             const pokemonInfo = `
                 <h2>${name}</h2>
@@ -24,6 +28,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // Update the content of the div with the Pokemon data
             pokemonDataElement.innerHTML = pokemonInfo;
+
+            // Set the src attribute of the image element to the Pokémon's image URL
+            pokemonImageElement.src = imageUrl;
         })
         .catch((error) => {
             console.error("Error fetching data:", error);
